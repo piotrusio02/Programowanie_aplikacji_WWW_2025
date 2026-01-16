6b26580cdc35b44203942d2e0574aafdcc7567f0
piotrusio02 <piotrxpiotrowski@gmail.com>
2026-01-16 01:32:26 +0100

    Zmiana nazwy folderu
    Zmiana nazwy folderu Lab_5 na Lab_05 dla poprawnego sortowania w serwisie GitHub.

---
+++ Lab_05/Projekt_1.5/labor_169351.php
@@ -0,0 +1,7 @@
+<?php
+    $nr_indexu = '169351';
+    $nGrupy = '3';
+
+    echo 'Piotr Piotrowski'.$nr_indexu.'gruppa'.$nrGrupy.'<br/><br/>';
+    echo 'Zastosowanie metody include() <br />';
+?>
--- Lab_5/Projekt_1.5/labor_169351.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $nr_indexu = '169351';
-    $nGrupy = '3';
-
-    echo 'Piotr Piotrowski'.$nr_indexu.'gruppa'.$nrGrupy.'<br/><br/>';
-    echo 'Zastosowanie metody include() <br />';
-?>
